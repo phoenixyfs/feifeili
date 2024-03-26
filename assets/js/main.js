bfa07877e4b6f1b142febdea05375cc3c85f1b52
phoenix <phoenixyfs@gmail.com>
2024-03-27 04:04:34 +0800

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
 //@prepros-prepend aos.js
+
+
+
+// Resize Logo when scroll
+
+// $(document).ready(function () {
+//     $(window).scroll(function () {
+//       $(".main-header").toggleClass("main-header-shrink", $(this).scrollTop() > 40)
+//     });
+// });
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        $(".logo").toggleClass("logo-shrink", $(this).scrollTop() > 20)
+    });
+});
